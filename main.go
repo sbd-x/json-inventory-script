0d078d0812c8de836cf296007127a5a74dd8bec0
Kelsey Hightower <kelsey.hightower@gmail.com>
2013-09-22 07:29:11 -0700

    Document how we handle the printing of the special all group

--- main.go
+++ main.go
@@ -73,6 +73,9 @@ func main() {
 	// described above.  Since group vars are optional, we only add them
 	// to the output of the group if non-empty.
 	for _, group := range groupPaths {
+
+		// The all group is only used for setting default group vars, it
+		// will not be part of the final output.
 		if group.Name() == "all.json" {
 			continue
 		}
